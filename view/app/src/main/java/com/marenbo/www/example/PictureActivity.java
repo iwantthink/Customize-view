1dd48e9ae44a08876188b34a8111dbba97f59c26
marenbo <my-chocolate@163.com>
2016-08-09 23:37:46 +0800

    status bar

--- view/app/src/main/java/com/marenbo/www/example/PictureActivity.java
+++ view/app/src/main/java/com/marenbo/www/example/PictureActivity.java
@@ -1,7 +1,11 @@
 package com.marenbo.www.example;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 
 public class PictureActivity extends AppCompatActivity {
@@ -9,10 +13,37 @@ public class PictureActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+//这种方法状态栏是半透明的
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+// setStatusBarColor在5.0以上的真机是可以完美实现透明状态栏的
+//        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        changeStatusBar();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
 
 
+    }
+
+    /**
+     *
+     */
+    private void changeStatusBar() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            //5.0 全透明实现
+//            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //4.4 全透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
     }
 
 
